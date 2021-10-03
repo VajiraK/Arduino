21f8cc91dc5909e741f47703158860af3a7354d2
Vajira Kulatunga <vajira.kulatunga@pearson.com>
2021-10-03 21:08:16 +0530

    WaterHeater_V4.ino changes

--- AtTiny/Water heater/WaterHeater/WaterHeater_V4.ino
+++ AtTiny/Water heater/WaterHeater/WaterHeater_V4.ino
@@ -3,32 +3,26 @@ WaterHeater_V4
 20 Sep 2021
 */
 
-const int 	sensorPin	=  A2;
+#define MAX_MASTER_COUNT 285
+
+int const sensorPin	=  A2;
 int const dataPin = PB0;//pin 0
 int const latchPin = PB1;//pin 1
 int const clockPin = PB2;//pin 2
+
+int masterCount = 0;
 //---------------------------------------
 void setup() {
   DDRB |= (1 << latchPin);//pinMode(latchPin, OUTPUT);
   DDRB |= (1 << dataPin);//pinMode(dataPin, OUTPUT);
   DDRB |= (1 << clockPin);//pinMode(clockPin, OUTPUT);
-}
-//---------------------------------------
-void loop() {
-
-  byte temp1 = getRightAdjustResult();
-  byte temp2 = getLeftAdjustResult();
-  printTwoDigits(temp1);
-  delay(1000);
-}
-//---------------------------------------
-byte getLeftAdjustResult(){
+  
   /*ADC Multiplexer Selection Register
   Bit 6 – REFS0: Reference Selection Bit
   Bit 5 – ADLAR: ADC Left Adjust Result
   Bits 1:0 – MUX[1:0]: Analog Channel Selection Bits*/
   ADMUX = B00100010;
-  
+
   /*ADC Control and Status Register A
   Bit 7 – ADEN: ADC Enable
   Bit 6 – ADSC: ADC Start Conversion
@@ -36,14 +30,40 @@ byte getLeftAdjustResult(){
   Bit 4 – ADIF: ADC Interrupt Flag
   Bit 3 – ADIE: ADC Interrupt Enable
   Bits 2:0 – ADPS[2:0]: ADC Prescaler Select Bits*/
-  ADCSRA = B11000110;
+  ADCSRA = B11000010;
+}
+//---------------------------------------
+void loop() {
+  
+  if(masterCount++ == MAX_MASTER_COUNT)
+  
+  //ADC Start Conversion
+  ADCSRA |= (1 << ADSC); 
+  
+  WaitForAdcComplete();
 
+  byte temp = getLeftAdjustResult();
+  temp = temp/10;
+  
+  if(temp > 9)
+    temp = 9;
+  
+  printNum(temp);
+    
+  delay(1000);
+}
+//---------------------------------------
+void WaitForAdcComplete()
+{
   //Wait till Bit 4 – ADIF is set(complete ADC)
   byte flag = 0;
   do{
   	flag = ADCSRA & B00010000;
   }while(flag == 0);
-  
+}
+//---------------------------------------
+byte getLeftAdjustResult(){
+
   //ADCL must be read first
   byte _ADCL = ADCL;
   byte _ADCH = ADCH; 
@@ -59,59 +79,20 @@ byte getLeftAdjustResult(){
   return toCelsius(rawData);
 }
 //---------------------------------------
-byte getRightAdjustResult(){
-  /*ADC Multiplexer Selection Register
-  Bit 6 – REFS0: Reference Selection Bit
-  Bit 5 – ADLAR: ADC Left Adjust Result
-  Bits 1:0 – MUX[1:0]: Analog Channel Selection Bits*/
-  ADMUX = B00000010;
-  
-  /*ADC Control and Status Register A
-  Bit 7 – ADEN: ADC Enable
-  Bit 6 – ADSC: ADC Start Conversion
-  Bit 5 – ADATE: ADC Auto Trigger Enable
-  Bit 4 – ADIF: ADC Interrupt Flag
-  Bit 3 – ADIE: ADC Interrupt Enable
-  Bits 2:0 – ADPS[2:0]: ADC Prescaler Select Bits*/
-  ADCSRA = B11000110;
-
-  //Wait till Bit 4 – ADIF is set(complete ADC)
-  byte flag = 0;
-  do{
-  	flag = ADCSRA & B00010000;
-  }while(flag == 0);
-  
-  //ADCL must be read first
-  byte _ADCL = ADCL;
-  byte _ADCH = ADCH; 
-
-  //First half
-  int rawData = 0;
-  rawData |= _ADCH;
-  rawData = rawData << 8;
-  //Second half
-  rawData |= _ADCL;
-  
-  return toCelsius(rawData);
-}
-//---------------------------------------
-//reads data, converts to degree celsius
 float toCelsius(int raw) {
+  //reads data, converts to degree celsius
   float volt = raw * 5.0;
   volt /= 1024.0;
   float temp = (volt - 0.5) * 100;
   return temp;
 }
 //---------------------------------------
-byte printTwoDigits(byte digits)
+byte printNum(byte digits)
 {
   PORTB &= ~(1 << latchPin);//digitalWrite(latchPin, LOW);
-  
-  byte n = getBits(digits%10);
-  shiftOut(dataPin, clockPin, MSBFIRST, n);
-  
-  n = getBits(digits/10);
-  shiftOut(dataPin, clockPin, MSBFIRST, n);
+
+  digits = getBits(digits);
+  shiftOut(dataPin, clockPin, MSBFIRST, digits);
   
   PORTB |= (1 << latchPin);//digitalWrite(latchPin, HIGH);
 }
